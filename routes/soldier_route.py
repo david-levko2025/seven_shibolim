db455844902096f1ac1351bc8845cc2b6fe1bb5e
david-levko2025 <mdavidl2003@gmail.com>
2025-11-26 17:59:49 +0200

    Update soldier_route.py

--- routes/soldier_route.py
+++ routes/soldier_route.py
@@ -7,3 +7,9 @@ from api.csv_load import deployed_soldiers
 def search_soldier_by_id_num(id_number):
     soldier_id_num = soldier_by_id_number(id_number)
     if soldier_id_num.deployed_soldiers:
+             respone = f"the soldier id {soldier_id_num.id_number} deployed"
+    else:
+        respone =f"the soldier id {soldier_id_num.id_number} in waiting list"
+    outcome = {"is deployed?": soldier_id_num.deployed_soldiers, "messege": respone}
+    return outcome
+    
