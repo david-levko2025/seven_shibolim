031ee967c1fd652d7ba4d7d0dbd58852f6208e6a
My Name <mdavidl@gmail.com>
2025-11-26 16:32:36 +0200

    change in the route

--- api/route.py
+++ api/route.py
@@ -4,23 +4,40 @@ import csv
 from util.residential_base import Residential_base
 from util.bublle_sort import Sort
 from api.gen import app
+from sqlmodel import Session
+from dal.engine import engine
+from models.info_soldiers import Soldier
 
 
+def read_csv(content):
+    reader = csv.reader(io.StringIO(content))
+    header = next(reader)
+    rows = list(reader)
+    return rows
 
 @app.post("/assignWithCsv")
-def upload_csv_plus_sort(file: UploadFile):
+def upload_csv(file: UploadFile):
     if file.content_type != "text/csv":
          return {"error": "File must be a CSV"}
     
     content = file.file.read().decode("utf-8")
-    reader = csv.reader(io.StringIO(content))
-    header = next(reader)
-    rows = list(reader)
+    rows = read_csv(content)
+    with Session(engine) as session:
+        for line in rows:
+            soldier = Soldier(id_number = line[0],
+                             f_name = line[1],
+                             l_name = line[2],
+                             gender = line[3],
+                             city = line[4],
+                             his_distance = line[5],
+                             )
+            session.add(soldier)
+            session.commit()
+            
+    
     Sort.sort_on_distance(rows)
     distribution_by_residence = Residential_base.bases(rows)
     deployed_soldiers = len(distribution_by_residence['base_one']) + len(distribution_by_residence['base_two'])
-    # for line in rows:
-    #     print(line)
 
     return {
         "the number of soldiers deployed ":deployed_soldiers,
